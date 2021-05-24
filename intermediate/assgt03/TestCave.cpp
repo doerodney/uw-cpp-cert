2199c3197f95da70abbf9ffaf5f26edb71a5f97c
Rod Doe <doerodney@gmail.com>
2021-05-23 17:07:39 -0700

    Added denizen detection to cave test.

--- intermediate/assgt03/TestCave.cpp
+++ intermediate/assgt03/TestCave.cpp
@@ -18,13 +18,22 @@ namespace TestHuntTheWumpus
 
     // Create a Bat to move in and out of the cave:
     const HuntTheWumpus::Bat bat(0, env.m_context);
+    const auto b = std::make_shared<HuntTheWumpus::Bat>(0, env.m_context);
 
     // Create a Cave:
     HuntTheWumpus::Cave cave(1, dungeon);
 
     TEST(CaveSuite, Cave_HasDenizens)
     {
+        // Verify that new cave has no denizens:
         CHECK_EQUAL(false, cave.HasDenizens());
+
+        // Add a denizen to the cave:
+        cave.AddDenizen(b, true);
+
+        // Verify that the cave now has a denizen:
+        CHECK_EQUAL(true, cave.HasDenizens());
+
     }
 
 }
