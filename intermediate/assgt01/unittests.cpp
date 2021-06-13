6b988891ffb2cca41d9998e00fe6bc404de07eda
Rod <doerodney@gmail.com>
2021-06-13 12:31:02 -0700

    assgt01: Fixed alignments in unittests.cpp.

--- intermediate/assgt01/unittests.cpp
+++ intermediate/assgt01/unittests.cpp
@@ -9,123 +9,123 @@
 
 namespace TestHuntTheWumpus
 {
-	TEST(ArrowSuite, Arrow_HasProperAttributes)
-    {
-        const HuntTheWumpus::Arrow arrow(0);
+  TEST(ArrowSuite, Arrow_HasProperAttributes)
+  {
+    const HuntTheWumpus::Arrow arrow(0);
 
-        const auto &properties = arrow.Properties();
+    const auto &properties = arrow.Properties();
 
-        CHECK(!properties.m_carryableByBats);
-        CHECK(properties.m_fatalToWumpus);
-		CHECK(properties.m_fatalToHunter);
-		CHECK(!properties.m_isEdible);
-        CHECK(!properties.m_reportMovement);
-        
-        const auto& id = arrow.GetIdentifier();
+    CHECK(!properties.m_carryableByBats);
+    CHECK(properties.m_fatalToWumpus);
+    CHECK(properties.m_fatalToHunter);
+    CHECK(!properties.m_isEdible);
+    CHECK(!properties.m_reportMovement);
 
-        CHECK_EQUAL(HuntTheWumpus::Category::Arrow, id.m_category);
-    }
+    const auto& id = arrow.GetIdentifier();
 
-	TEST(BatSuite, Bat_HasProperAttributes)
-    {
-        const HuntTheWumpus::Bat bat(0);
+    CHECK_EQUAL(HuntTheWumpus::Category::Arrow, id.m_category);
+  }
 
-        const auto &properties = bat.Properties();
+  TEST(BatSuite, Bat_HasProperAttributes)
+  {
+    const HuntTheWumpus::Bat bat(0);
 
-        CHECK(!properties.m_carryableByBats);
-        CHECK(!properties.m_fatalToWumpus);
-		CHECK(!properties.m_fatalToHunter);
-		CHECK(!properties.m_isEdible);
-        CHECK(!properties.m_reportMovement);
-        
-        const auto& id = bat.GetIdentifier();
+    const auto &properties = bat.Properties();
 
-        CHECK_EQUAL(HuntTheWumpus::Category::Bat, id.m_category);
-    }
+    CHECK(!properties.m_carryableByBats);
+    CHECK(!properties.m_fatalToWumpus);
+    CHECK(!properties.m_fatalToHunter);
+    CHECK(!properties.m_isEdible);
+    CHECK(!properties.m_reportMovement);
 
-	TEST(HunterSuite, Hunter_HasProperAttributes)
-    {
-        const HuntTheWumpus::Hunter hunter(0);
+    const auto& id = bat.GetIdentifier();
 
-        const auto &properties = hunter.Properties();
+    CHECK_EQUAL(HuntTheWumpus::Category::Bat, id.m_category);
+  }
 
-        CHECK(properties.m_carryableByBats);
-        CHECK(!properties.m_fatalToWumpus);
-		CHECK(!properties.m_fatalToHunter);
-		CHECK(properties.m_isEdible);
-        CHECK(properties.m_reportMovement);
-        
-        const auto& id = hunter.GetIdentifier();
+  TEST(HunterSuite, Hunter_HasProperAttributes)
+  {
+    const HuntTheWumpus::Hunter hunter(0);
 
-        CHECK_EQUAL(HuntTheWumpus::Category::Hunter, id.m_category);
-    }
+    const auto &properties = hunter.Properties();
 
-	TEST(PitSuite, Pit_HasProperAttributes)
-    {
-        const HuntTheWumpus::Pit pit(0);
+    CHECK(properties.m_carryableByBats);
+    CHECK(!properties.m_fatalToWumpus);
+    CHECK(!properties.m_fatalToHunter);
+    CHECK(properties.m_isEdible);
+    CHECK(properties.m_reportMovement);
 
-        const auto &properties = pit.Properties();
+    const auto& id = hunter.GetIdentifier();
 
-        CHECK(!properties.m_carryableByBats);
-        CHECK(!properties.m_fatalToWumpus);
-		CHECK(properties.m_fatalToHunter);
-		CHECK(!properties.m_isEdible);
-        CHECK(!properties.m_reportMovement);
-        
-        const auto& id = pit.GetIdentifier();
+    CHECK_EQUAL(HuntTheWumpus::Category::Hunter, id.m_category);
+  }
 
-        CHECK_EQUAL(HuntTheWumpus::Category::Pit, id.m_category);
-    }
+  TEST(PitSuite, Pit_HasProperAttributes)
+  {
+    const HuntTheWumpus::Pit pit(0);
+
+    const auto &properties = pit.Properties();
+
+    CHECK(!properties.m_carryableByBats);
+    CHECK(!properties.m_fatalToWumpus);
+    CHECK(properties.m_fatalToHunter);
+    CHECK(!properties.m_isEdible);
+    CHECK(!properties.m_reportMovement);
+
+    const auto& id = pit.GetIdentifier();
+
+    CHECK_EQUAL(HuntTheWumpus::Category::Pit, id.m_category);
+  }
 
 
-    TEST(WumpusSuite, Wumpus_HasProperAttributes)
-    {
-        const HuntTheWumpus::Wumpus wumpus(0);
+  TEST(WumpusSuite, Wumpus_HasProperAttributes)
+  {
+    const HuntTheWumpus::Wumpus wumpus(0);
 
-        const auto &properties = wumpus.Properties();
+    const auto &properties = wumpus.Properties();
 
-        CHECK(!properties.m_carryableByBats);
-        CHECK(!properties.m_fatalToWumpus);
-		CHECK(properties.m_fatalToHunter);
-		CHECK(!properties.m_isEdible);
-        CHECK(!properties.m_reportMovement);
-        
-        const auto& id = wumpus.GetIdentifier();
+    CHECK(!properties.m_carryableByBats);
+    CHECK(!properties.m_fatalToWumpus);
+    CHECK(properties.m_fatalToHunter);
+    CHECK(!properties.m_isEdible);
+    CHECK(!properties.m_reportMovement);
 
-        CHECK_EQUAL(HuntTheWumpus::Category::Wumpus, id.m_category);
-    }
+    const auto& id = wumpus.GetIdentifier();
 
-	TEST(DenizenSuite, TestIdentifier_Comparison_ProperSorting)
-    {
-        const HuntTheWumpus::DenizenIdentifier thing1 { HuntTheWumpus::Category::Wumpus, 0 };  // Wumpus == 0 
-        const HuntTheWumpus::DenizenIdentifier thing2 { HuntTheWumpus::Category::Arrow, 0 };   // Arrow == 4
-        const HuntTheWumpus::DenizenIdentifier thing3 { HuntTheWumpus::Category::Arrow, 1 };
+    CHECK_EQUAL(HuntTheWumpus::Category::Wumpus, id.m_category);
+  }
 
-        CHECK( thing1 <=> thing2 == std::strong_ordering::less);
-        CHECK( thing2 <=> thing1 == std::strong_ordering::greater);
-        CHECK( thing2 <=> thing3 == std::strong_ordering::less);
-        CHECK( thing3 <=> thing2 == std::strong_ordering::greater);
-        CHECK( thing3 <=> thing3 == std::strong_ordering::equal);
+  TEST(DenizenSuite, TestIdentifier_Comparison_ProperSorting)
+  {
+    const HuntTheWumpus::DenizenIdentifier thing1 { HuntTheWumpus::Category::Wumpus, 0 };  // Wumpus == 0 
+    const HuntTheWumpus::DenizenIdentifier thing2 { HuntTheWumpus::Category::Arrow, 0 };   // Arrow == 4
+    const HuntTheWumpus::DenizenIdentifier thing3 { HuntTheWumpus::Category::Arrow, 1 };
 
-        CHECK( thing3 == thing3 );
-    }
+    CHECK( thing1 <=> thing2 == std::strong_ordering::less);
+    CHECK( thing2 <=> thing1 == std::strong_ordering::greater);
+    CHECK( thing2 <=> thing3 == std::strong_ordering::less);
+    CHECK( thing3 <=> thing2 == std::strong_ordering::greater);
+    CHECK( thing3 <=> thing3 == std::strong_ordering::equal);
 
-    TEST(DenizenSuite, DenizenIdentifier_Hashing_UniqueHashes)
-    {
-        const HuntTheWumpus::DenizenIdentifier thing1 { HuntTheWumpus::Category::Arrow, 0 };
-        const HuntTheWumpus::DenizenIdentifier thing2 { HuntTheWumpus::Category::Bat, 0 };
-        const HuntTheWumpus::DenizenIdentifier thing3 { HuntTheWumpus::Category::Bat, 1 };
-        const HuntTheWumpus::DenizenIdentifier thing4 { HuntTheWumpus::Category::Bat, 1 };
+    CHECK( thing3 == thing3 );
+  }
 
-        const HuntTheWumpus::DenizenIdentifierHasher hasher;
+  TEST(DenizenSuite, DenizenIdentifier_Hashing_UniqueHashes)
+  {
+    const HuntTheWumpus::DenizenIdentifier thing1 { HuntTheWumpus::Category::Arrow, 0 };
+    const HuntTheWumpus::DenizenIdentifier thing2 { HuntTheWumpus::Category::Bat, 0 };
+    const HuntTheWumpus::DenizenIdentifier thing3 { HuntTheWumpus::Category::Bat, 1 };
+    const HuntTheWumpus::DenizenIdentifier thing4 { HuntTheWumpus::Category::Bat, 1 };
 
-        const auto hash1 = hasher(thing1);
-        const auto hash2 = hasher(thing2);
-        const auto hash3 = hasher(thing3);
-        const auto hash4 = hasher(thing4);
+    const HuntTheWumpus::DenizenIdentifierHasher hasher;
 
-        CHECK( hash1 != hash2 );
-        CHECK( hash2 != hash3 );
-        CHECK_EQUAL(hash3, hash4);
-    }
+    const auto hash1 = hasher(thing1);
+    const auto hash2 = hasher(thing2);
+    const auto hash3 = hasher(thing3);
+    const auto hash4 = hasher(thing4);
+
+    CHECK( hash1 != hash2 );
+    CHECK( hash2 != hash3 );
+    CHECK_EQUAL(hash3, hash4);
+  }
 }
